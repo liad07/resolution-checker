a40f49c1f1ff0d816885cd23c9855a89d7c6f33f
liadk07 <91607695+liad07@users.noreply.github.com>
2022-04-01 15:40:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function resulaition() {
+    var x=document.getElementById('text').innerHTML="the width is: "+screen.width;
+    var y=document.getElementById('text2').innerHTML="the height is: "+screen.height;
+}
